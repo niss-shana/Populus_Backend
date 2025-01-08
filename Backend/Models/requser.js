9e1389c3454f2c6334a47661c3ebca856cfba735
niss-shana <134854766+niss-shana@users.noreply.github.com>
2025-01-08 23:16:59 +0530

    sign up done

--- Backend/Models/requser.js
+++ Backend/Models/requser.js
@@ -4,18 +4,18 @@ const residentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   dateOfBirth: { type: String, required: true },
   gender: { type: String, required: true },
-  houseNumber: { type: String, required: true },
+  houseDetails: { type: String, required: true }, 
   place: { type: String, required: true },
   locality: { type: String, required: true },
   district: { type: String, required: true },
-  mobileNumber: { type: String, required: true },
-  aadhaarNumber: { type: String, required: true },
+  mobileNo: { type: String, required: true }, 
+  aadhaarNo: { type: String, required: true }, 
   rationId: { type: String, required: true },
   photo: { type: String },
   mappedHouse: { type: String },
-  ward: { type: String, required: true },
   verified: { type: Boolean, default: false },
 });
 
-const Resident = mongoose.model('Resident', residentSchema);
-export default Resident;
+
+const RequestUsers = mongoose.model('RequestUsers', residentSchema);
+export default RequestUsers;
--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,14 +1,16 @@
 import express  from 'express';
-import Resident from '../models/requser.js';
+import RequestUsers from '../models/requser.js';
 const router = express.Router();
 
 router.post('/resident_signup', async (req, res) => {
   try {
     console.log("resident add");
     console.log(req.body);
-    const newResident = new Resident(req.body);
-    await newResident.save();
+    const reqResident = new RequestUsers(req.body);
+    await reqResident.save();
     console.log(newResident);
+    const demo = await Resident.find();
+    console.log(demo);
     res.status(201).json({message:"Message successfuly"});
   } catch (error) {
     res.status(400).json({ error: error.message });
