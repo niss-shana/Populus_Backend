b86657cca65a213ce004daf7c525bd17574b2079
swarimabdussamad <swarimabdussamad@gmail.com>
2025-01-14 08:42:27 +0530

    unverified users route added in governmentRoute

--- Backend/Routes/governmentRoute.js
+++ Backend/Routes/governmentRoute.js
@@ -10,4 +10,35 @@ router.post('/login', async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+router.post('/signup', async (req, res) => {
+  try {
+    console.log(req.body)
+   
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/unverified-users', async (req, res) => {
+  try {
+    const users = await RequestUsers.find({ isVerified: false });
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default router
