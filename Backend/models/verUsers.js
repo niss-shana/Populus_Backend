2ff8c38f3e5b3c043931cc29a6c90816c18cad79
MOHAMMED LABEEB A P <labeebap446@gmail.com>
2025-03-09 18:01:24 +0530

    age

--- Backend/models/verUsers.js
+++ Backend/models/verUsers.js
@@ -4,22 +4,22 @@ const residentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   dateOfBirth: { type: String, required: true },
   gender: { type: String, required: true },
-  houseDetails: { type: String, required: true }, 
+  houseDetails: { type: String, required: true },
   place: { type: String },
   locality: { type: String },
   district: { type: String, required: true },
-  mobileNo: { type: String, required: true }, 
-  aadhaarNo: { type: String, required: true }, 
+  mobileNo: { type: String, required: true },
+  aadhaarNo: { type: String, required: true },
   rationId: { type: String, required: true },
   photo: { type: String },
   mappedHouse: { type: String },
-  username: {type: String, required: true},
-  password: {type: String, required: true},
-  presidentId: {  type: String},
-  isOwnerHome: {type: String}, 
-  
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  presidentId: { type: String },
+  isOwnerHome: { type: String },
 });
 
+// Check if the model already exists
+const VerifiedUsers = mongoose.models.VerifiedUsers || mongoose.model('VerifiedUsers', residentSchema);
 
-const VerifiedUsers = mongoose.model('VerifiedUsers', residentSchema);
 export default VerifiedUsers;
